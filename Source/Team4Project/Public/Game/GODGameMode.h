96376e22b8227141cebf4b07bdd3a83868fe8b29
markpark65 <jhmark65@naver.com>
2026-06-19 20:53:37 +0900

    feat: GameMode, GameState, and PlayerState

---
+++ Source/Team4Project/Public/Game/GODGameMode.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GODGameState.h"
+#include "GODGameMode.generated.h"
+
+UCLASS()
+class TEAM4PROJECT_API AGODGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	AGODGameMode();
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Game Logic")
+	void StartGame();
+
+	UFUNCTION(BlueprintCallable, Category = "Game Logic")
+	void StartSeamlessTravelToGame();
+
+	UFUNCTION(BlueprintCallable, Category = "Game Logic")
+	void HandlePlayerDeath(class AGODPlayerState* KillerPS, class AGODPlayerState* VictimPS);
+
+	UFUNCTION(BlueprintCallable, Category = "Game Logic")
+	void TriggerDerailment();
+
+private:
+	FTimerHandle GameTimerHandle;
+
+	void UpdateGameTimer();
+	void AssignRoles();
+	void CheckWinConditions();
+	void EndGame(enum EGamePhase WinningPhase);
+
+	int32 TotalMatchTime = 600; // 10분 (600초)
+	int32 TimeElapsed = 0;      // 진행 시간 추적용
+
+};
---
+++ Source/Team4Project/Public/Game/GODGameState.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "GODGameState.generated.h"
+
+UENUM(BlueprintType)
+enum class EGamePhase : uint8
+{
+	WaitingForPlayers UMETA(DisplayName = "대기 중"),
+	Playing UMETA(DisplayName = "진행 중"),
+	CitizensWon UMETA(DisplayName = "보안관/시민 승리"),
+	OutlawWon UMETA(DisplayName = "무법자 승리"),
+	MafiaWon UMETA(DisplayName = "마피아 승리")
+};
+
+UCLASS()
+class TEAM4PROJECT_API AGODGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+public:
+	AGODGameState();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(ReplicatedUsing = OnRep_GamePhase, BlueprintReadOnly, Category = "Game State")
+	EGamePhase CurrentPhase;
+
+	UPROPERTY(Replicated, BlueprintReadOnly, Category = "Game State")
+	int32 RemainingTime;
+
+	UPROPERTY(Replicated, BlueprintReadOnly, Category = "Game State")
+	float DistanceToDestination;
+
+	// 총기 사용 가능 여부 (게임 시작 3분 후 True)
+	UPROPERTY(Replicated, BlueprintReadOnly, Category = "Game State")
+	bool bGunsUnlocked;
+
+	UFUNCTION()
+	void OnRep_GamePhase();
+};
