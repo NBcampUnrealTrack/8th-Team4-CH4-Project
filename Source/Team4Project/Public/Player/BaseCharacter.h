a9488ce63ee79216f48787953ab6f4ba5237d244
kimdaehyum <129298858+kimdaehyum@users.noreply.github.com>
2026-07-16 00:13:34 +0900

    [대현] - Climb InputAction 제거
     c++ Climb InputAction 제거, F 상호작용으로 통일

--- Source/Team4Project/Public/Player/BaseCharacter.h
+++ Source/Team4Project/Public/Player/BaseCharacter.h
@@ -762,9 +762,6 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* LookAction;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* ClimbAction;
-
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	UCustomMovementComponent* CustomMovementComponent;
 
